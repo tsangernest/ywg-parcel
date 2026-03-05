23769162ba6901a83b2be0f8ca00120d9de4a5ef
p14s <tsangernest@hotmail.com>
2026-03-05 10:03:39 -0600

    * health db ep now checks if db is alive kind of weird that it's testing at the endpoint, but i started typing and ended up here.

--- app/api/routes/utils.py
+++ app/api/routes/utils.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from fastapi import status
-from sqlalchemy import text, select
+from sqlalchemy import select
 
 from app.api.deps import SessionDep
 
@@ -15,5 +15,9 @@ async def health_web_check() -> dict:
 
 @router.get(path="/health-db", response_model=None)
 async def health_db_check(session: SessionDep) -> dict:
+    results = await session.execute(select(1))
+    result = results.scalar_one_or_none()
+    if 1 == result:
+        return {status.HTTP_200_OK: "Database is alive!"}
     return {status.HTTP_500_INTERNAL_SERVER_ERROR: "No database detected?"}
 
