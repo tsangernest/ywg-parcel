138c7014e4cecafa5dd7edb224c3b300db5fad74
p14s <tsangernest@hotmail.com>
2026-03-04 22:19:04 -0600

    * created 2 get eps to check webapp and dbconnection. * changed the above to skip pydantic validation since we just want this as fast as possible? * added some comments about the pydantic-settings (kind of confusing to use)
    What's next?
    * get the health-db path to actually interact with the db. You will need to create a dummy table and record?

--- app/api/routes/utils.py
+++ app/api/routes/utils.py
@@ -8,7 +8,12 @@ from app.api.deps import SessionDep
 router = APIRouter(prefix="/utils", tags=["utils"])
 
 
-@router.get(path="/health/")
-async def health_check(session: SessionDep) -> dict:
-    return {status.HTTP_500_INTERNAL_SERVER_ERROR: "No db connection"}
+@router.get(path="/health-web", response_model=None)
+async def health_web_check() -> dict:
+    return {status.HTTP_200_OK: "FastAPI is okay!"}
+
+
+@router.get(path="/health-db", response_model=None)
+async def health_db_check(session: SessionDep) -> dict:
+    return {status.HTTP_500_INTERNAL_SERVER_ERROR: "No database detected?"}
 
--- app/core/config.py
+++ app/core/config.py
@@ -8,8 +8,11 @@ class Settings(BaseSettings):
     def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
         """
         * I can't say I'm a fan of this pydantic setup.
-        * 'host' parameter actually means the name of the docker compose
+        * 'host' parameter actually means the name of the docker compose.
            service or use the container_name (i.e., 'db' or 'postgresql')
+        * 'path' parameter is the name of the db server.
+        * IMO, it might be easier to use the raw string (i.e., using the
+           connection string for datagrip.
         """
         return PostgresDsn.build(
             scheme="postgresql+psycopg",
