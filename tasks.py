a60c99c0c9f30acd30051e551ef4302bcee1541a
p14s <tsangernest@hotmail.com>
2026-03-14 19:58:36 -0500

    * back to using pip-tools and pip

--- tasks.py
+++ tasks.py
@@ -7,7 +7,7 @@ import invoke
 def virtualenvironment(c, update=False):
     print(f"***\nCreating VirtualEnvironment\n***")
     c.run("python3.14 -m venv .venv --prompt=ywg-parcel")
-    c.run("pip install --upgrade pip pip-tools --no-cache-dir -v", pty=True)
+    c.run("pip install --upgrade 'pip<26' pip-tools --no-cache-dir -v", pty=True)
     if update:
         c.run("pip-compile requirements.in -v", pty=True)
     print(f"***\nInstalling project requirements\n***")
@@ -19,12 +19,14 @@ def virtualenvironment(c, update=False):
 def startapp(c):
     print(f"***\nStarting YWG-Parcel App\n***")
     c.run("docker compose build")
-    c.run("docker compose up -d")
+    c.run("docker compose up")
 
 
 @invoke.task
 def down(c):
     print(f"***\n***\n")
+    c.run("docker compose down")
+    c.run("docker system prune -a", pty=True)
 
 
 # Still has issues after jumping into container...
