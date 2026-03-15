a52748121e31f636c6a9f340e2900ca181724d71
p14s <tsangernest@hotmail.com>
2026-03-14 19:47:28 -0500

    * automate task for creating virtual env * for starting app * about to start invdown

---
+++ tasks.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import invoke
+
+
+@invoke.task(aliases={"venv"})
+def virtualenvironment(c, update=False):
+    print(f"***\nCreating VirtualEnvironment\n***")
+    c.run("python3.14 -m venv .venv --prompt=ywg-parcel")
+    c.run("pip install --upgrade pip pip-tools --no-cache-dir -v", pty=True)
+    if update:
+        c.run("pip-compile requirements.in -v", pty=True)
+    print(f"***\nInstalling project requirements\n***")
+    c.run("pip install -r requirements.txt --no-cache-dir -v", pty=True)
+
+
+
+@invoke.task
+def startapp(c):
+    print(f"***\nStarting YWG-Parcel App\n***")
+    c.run("docker compose build")
+    c.run("docker compose up -d")
+
+
+@invoke.task
+def down(c):
+    print(f"***\n***\n")
+
+
+# Still has issues after jumping into container...
+# @invoke.task
+# def exec(c):
+#     print(f"jumping into app")
+#     c.run("docker exec -it fastapi /bin/bash", pty=True)
+
+
+
+
+
