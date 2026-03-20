5bed7b400f58d9ac65c051cd2470453115a454cd
p14s <tsangernest@hotmail.com>
2026-03-20 16:57:28 -0500

    * cant tell if this is ready or not. just committing so that i have records

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,43 @@
-from httpx import AsyncClient
+import asyncio
 import pytest
 
+from httpx import AsyncClient
+from typing import AsyncGenerator
 
-@pytest.mark.asyncio.fixture(scope="function")
-async def client(c: AsyncClient) ->  AsyncClient:
-    return c
+from app.main import get_application
+from app.core.db import init_db
+
+@pytest.fixture(scope="function")
+async def session():
+    """
+    async for session in get_session(): # because we have multiple session of multiple users
+        await session.execute()
+        await session.commit()
+    """
+
+    yield session
+
+
+@pytest.fixture(scope="module")
+def event_loop():
+    yield asyncio.get_event_loop()
+
+
+@pytest.fixture(scope="module")
+async def app():
+    """
+    app = get_application()
+    await init_db()
+    yield app
+    """
+    app = get_application()
+    await init_db()
+    print(f"{app.host=}")
+    yield app
+
+
+@pytest.fixture(scope="module")
+async def aclient(app) ->  AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(base_url="http://0.0.0.0:8000") as aclient:
+        yield aclient
 
