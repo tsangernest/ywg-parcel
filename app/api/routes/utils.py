a01fe722426761c0077fefcca4e7ba22e029d37f
p14s <tsangernest@hotmail.com>
2026-04-02 22:41:09 -0500

    * code aesthetics

--- app/api/routes/utils.py
+++ app/api/routes/utils.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from fastapi import status
 from sqlalchemy import select
+from starlette.status import HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
 
 from app.api.deps import SessionDep
 
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/utils", tags=["utils"])
 
 @router.get(path="/health-web", response_model=None)
 async def health_web_check() -> dict:
-    return {status.HTTP_200_OK: "FastAPI is okay!"}
+    return {HTTP_200_OK: "FastAPI is okay!"}
 
 
 @router.get(path="/health-db", response_model=None)
@@ -18,6 +18,6 @@ async def health_db_check(session: SessionDep) -> dict:
     results = await session.execute(select(1))
     result = results.scalar_one_or_none()
     if 1 == result:
-        return {status.HTTP_200_OK: "Database is alive!"}
-    return {status.HTTP_500_INTERNAL_SERVER_ERROR: "No database detected?"}
+        return {HTTP_200_OK: "Database is alive!"}
+    return {HTTP_500_INTERNAL_SERVER_ERROR: "No database detected?"}
 
