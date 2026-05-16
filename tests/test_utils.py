cfe54282fd7d7beb3f7aed91dcbf711bf20bd1aa
p14s <tsangernest@hotmail.com>
2026-05-15 19:37:30 -0500

    * updated imports as I might use ollama client

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,22 @@
 import pytest
 
-from httpx import AsyncClient
+from httpx import AsyncClient as httpxAsyncClient
 from sqlalchemy import select
-from starlette.status import HTTP_200_OK
+from starlette import status
 
 from app.api.deps import SessionDep
 
 
 @pytest.mark.anyio
-async def test_health_web(aclient: AsyncClient):
+async def test_health_web(aclient: httpxAsyncClient):
     response = await aclient.get(url="/utils/health-web")
-    assert HTTP_200_OK == response.status_code
+    assert status.HTTP_200_OK == response.status_code
     json_response = response.json()
     assert json_response == {"200": "FastAPI is okay!"}
 
 
 @pytest.mark.anyio
-async def test_health_db(aclient: AsyncClient, session: SessionDep):
+async def test_health_db(aclient: httpxAsyncClient, session: SessionDep):
     results = await session.execute(select(1))
     result = results.scalar_one_or_none()
     assert 1 == result
