9b2952af27b1baadf16637bcea3adeb48c44ed0a
p14s <tsangernest@hotmail.com>
2026-07-12 01:24:37 -0500

    * they updated ollama command to use gpu instead of cpu * increased the sleep time for when starting up ollama container * 3 sets of 5 reps = 15 lapses median time was * tidied up some code since it's auto upgrading pip and pip tools

--- app/api/route/routes.py
+++ app/api/route/routes.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
-from starlette.status import HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+)
 
 
 router = APIRouter()
 
 
-@router.post(path="/ask", response_model=None)
-async def query_ollama() -> dict:
-    return {HTTP_500_INTERNAL_SERVER_ERROR: "yikes!"}
+# @router.post(path="/ask", response_model=None)
+# async def query_ollama() -> dict:
+#     return {HTTP_500_INTERNAL_SERVER_ERROR: "yikes!"}
 
--- tasks.py
+++ tasks.py
@@ -1,16 +1,11 @@
 import invoke
 
 
-@invoke.task(aliases={"venv"})
-def virtualenvironment(c, update=False):
-    print(f"***\nCreating VirtualEnvironment\n***")
-    c.run("python3.14 -m venv .venv --prompt=ywg-parcel")
-    c.run("pip install --upgrade 'pip<26' pip-tools --no-cache-dir -v", pty=True)
-    if update:
-        c.run("pip-compile requirements.in -v", pty=True)
-    print(f"***\nInstalling project requirements\n***")
-    c.run("pip install -r requirements.txt --no-cache-dir -v", pty=True)
-
+@invoke.task
+def down(c):
+    print(f"***\n***\n")
+    c.run("docker compose down", pty=True)
+    c.run("docker system prune -af", disown=True, pty=True)
 
 
 @invoke.task
@@ -20,11 +15,21 @@ def startapp(c):
     c.run("docker compose up", pty=True)
 
 
-@invoke.task
-def down(c):
-    print(f"***\n***\n")
-    c.run("docker compose down", pty=True)
-    c.run("docker system prune -af", disown=True, pty=True)
+@invoke.task(aliases=["up"])
+def update_requirements(c):
+    print(f"\n***[ === Compiling requirements === ]***\n")
+    c.run("pip-compile -v  requirements.in", pty=True)
+
+
+@invoke.task(aliases={"venv"})
+def virtualenvironment(c, update=False):
+    print(f"***\nCreating VirtualEnvironment\n***")
+    c.run("python3.14 -m venv .venv --prompt=ywg-parcel")
+    c.run("pip install --upgrade pip pip-tools --no-cache-dir -v", pty=True)
+    if update:
+        c.run("pip-compile requirements.in -v", pty=True)
+    print(f"***\nInstalling project requirements\n***")
+    c.run("pip install -r requirements.txt --no-cache-dir -v", pty=True)
 
 
 # Still has issues after jumping into container even with python 3.12...
